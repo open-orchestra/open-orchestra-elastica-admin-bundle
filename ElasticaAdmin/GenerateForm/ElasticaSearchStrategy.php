599ed397c647f2274cb25ced0f479a90c1e3b3fd
itkg-nanne <nicolas.anne@businessdecision.com>
2017-10-13 11:43:01 +0200

    use special page for elastica search block

--- ElasticaAdmin/GenerateForm/ElasticaSearchStrategy.php
+++ ElasticaAdmin/GenerateForm/ElasticaSearchStrategy.php
@@ -2,8 +2,10 @@
 
 namespace OpenOrchestra\ElasticaAdmin\GenerateForm;
 
+use OpenOrchestra\Backoffice\Context\ContextBackOfficeInterface;
 use OpenOrchestra\Backoffice\GenerateForm\Strategies\AbstractBlockStrategy;
 use OpenOrchestra\ModelInterface\Model\BlockInterface;
+use OpenOrchestra\ModelInterface\Repository\NodeRepositoryInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -12,6 +14,25 @@ use Symfony\Component\Validator\Constraints\NotBlank;
  */
 class ElasticaSearchStrategy extends AbstractBlockStrategy
 {
+    protected $nodeRepository;
+    protected $specialPageList;
+    protected $contextManager;
+
+    /**
+     * @param NodeRepositoryInterface     $nodeRepository,
+     * @param ContextBackOfficeInterface  $contextManager,
+     * @param array                       $basicBlockConfiguration
+     */
+    public function __construct(
+        NodeRepositoryInterface $nodeRepository,
+        ContextBackOfficeInterface $contextManager,
+        array $basicBlockConfiguration
+    ) {
+        $this->nodeRepository = $nodeRepository;
+        $this->contextManager = $contextManager;
+        parent::__construct($basicBlockConfiguration);
+    }
+
     /**
      * @param BlockInterface $block
      *
@@ -27,12 +48,31 @@ class ElasticaSearchStrategy extends AbstractBlockStrategy
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('contentNodeId', 'oo_node_choice', array(
+        $builder->add('contentNodeId', 'choice', array(
+            'choices' => $this->getSpecialPageList(),
             'label' => 'open_orchestra_elastica_admin.form.elastica_search.node',
             'constraints' => new NotBlank(),
             'group_id' => 'data',
             'sub_group_id' => 'content',
         ));
+    }
+
+    /**
+     * get special pages list
+     *
+     * @return array
+     */
+    protected function getSpecialPageList() {
+        $siteId = $this->contextManager->getSiteId();
+        $language = $this->contextManager->getSiteDefaultLanguage();
+        $specialPages = $this->nodeRepository->findAllSpecialPage($language, $siteId);
+
+        $specialPageChoice = array();
+        foreach ($specialPages as $node) {
+            $specialPageChoice[$node->getId()] = $node->getName();
+        }
+
+        return $specialPageChoice;
     }
 
     /**
